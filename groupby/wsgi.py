2d412524654e5e271c07df4851b7591537313c95
SeungHyeon <jshsakura@gmail.com>
2016-08-10 23:42:49 +0900

    test

--- groupby/wsgi.py
+++ groupby/wsgi.py
@@ -6,11 +6,11 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
-
 import os
+import sys
 
-from django.core.wsgi import get_wsgi_application
+sys.path.append('/work_test/groupby')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'groupby.settings'
+import django.core.handlers.wsgi
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "groupby.settings")
-
-application = get_wsgi_application()
+application = django.core.handlers.wsgi.WSGIHandler()
